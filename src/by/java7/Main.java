909f4b484d97e43082819516c3a98fa463f80b6d
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2013-12-25 10:06:53 +0300

    Added main class

---
+++ src/by/java7/Main.java
@@ -0,0 +1,13 @@
+package by.java7;
+
+import java.io.IOException;
+
+/**
+ * @author Tsimafei_Shchytkavets
+ */
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+    }
+}
